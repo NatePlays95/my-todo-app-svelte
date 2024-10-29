9006c3d6a0d096023ab7f2d0124c143bb529c867
Natan Maia <natanmaiaplays@outlook.com>
2024-10-29 13:05:25 -0300

    basic task list

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,45 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script>
+import { onMount } from "svelte";
+
+let currentTask = $state("");
+let taskList = $state([]);
+
+$inspect("currentTask:",currentTask);
+$inspect("taskList:",taskList);
+
+function addTask() {
+    taskList.push(currentTask);
+    currentTask = "";
+};
+
+function deleteTask(index) {
+    taskList = taskList.filter((_, i) => i !== index);
+};
+
+</script>
+
+
+
+<div>
+    <h1>To-Do List</h1>
+    <div>
+        <input
+            type="text"
+            bind:value={currentTask}
+        >
+        
+        <button
+            onclick={addTask}
+        >Add Task</button>
+
+    </div>
+
+    {#each taskList as task, index}
+        <div>
+            {task}
+            <button
+                onclick={() => {deleteTask(index)}}
+            >Delete</button>
+        </div>
+    {/each}
+</div>
